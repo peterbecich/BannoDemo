cf5e4a95e1b81249424652d96db28e60d2a592a9
Peter Becich <peterbecich@gmail.com>
2017-12-22 11:55:49 -0800

    print tweet handles to console with FS2

--- src/main/scala/me/peterbecich/bannodemo/twitter/TwitterQueue.scala
+++ src/main/scala/me/peterbecich/bannodemo/twitter/TwitterQueue.scala
@@ -8,8 +8,10 @@ import com.danielasfregola.twitter4s.TwitterStreamingClient
 import com.danielasfregola.twitter4s.entities.Tweet
 import com.danielasfregola.twitter4s.entities.streaming.StreamingMessage
 
+import cats._
+import cats.syntax.all._
 import cats.effect.{IO, Sync}
-import fs2.{io, text}
+import fs2.{io, text, Stream}
 import fs2.async.mutable.Queue
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -31,6 +33,8 @@ object TwitterQueue {
    https://oss.sonatype.org/service/local/repositories/releases/archive/co/fs2/fs2-core_2.12/0.9.6/fs2-core_2.12-0.9.6-javadoc.jar/!/fs2/async/mutable/Queue.html
 
    https://oss.sonatype.org/service/local/repositories/releases/archive/org/typelevel/cats-effect_2.12/0.5/cats-effect_2.12-0.5-javadoc.jar/!/cats/effect/IO.html
+
+   https://oss.sonatype.org/service/local/repositories/releases/archive/co/fs2/fs2-io_2.12/0.9.6/fs2-io_2.12-0.9.6-javadoc.jar/!/fs2/io/index.html
    */
 
   val createTwitterQueue: IO[Queue[IO, Tweet]] = Queue.unbounded[IO, Tweet]
@@ -42,11 +46,38 @@ object TwitterQueue {
       queue.enqueue1(tweet).unsafeRunAsync((e: Either[Throwable, Unit]) => ())
   }
 
-  val twitterStream: IO[Queue[IO, Tweet]] = for {
+  val createTwitterStream: IO[Stream[IO, Tweet]] = for {
     twitterQueue <- createTwitterQueue
     val foo = streamingClient.sampleStatuses()(unsafeTweetEnqueue(twitterQueue))
 
-  } yield twitterQueue
+  } yield twitterQueue.dequeue
+
+
+}
+
+object TwitterQueueExample {
+
+  // val printTwitter = for (
+  //   twitterStream <- TwitterQueue.createTwitterStream
+  //   val drained = twitterStream.map(_.text).take(64).through(text.utf8Encode).observe(io.stdout).drain.run
+  //   // val drained = twitterStream.map(_.text).through(text.utf8Encode).observe(io.stdout).drain
+  // ) yield drained  // TODO do this without yield
+
+  val printTwitter: IO[Unit] = TwitterQueue.createTwitterStream.flatMap { twitterStream =>
+    twitterStream
+      .map(t => t.user.map(_.name).getOrElse("nobody"))
+      .intersperse("\n")
+      // .through(text.lines)
+      .through(text.utf8Encode)
+      .observe(io.stdout)
+      .drain.run
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("twitter queue example, with FS2")
+
+    printTwitter.unsafeRunSync()
+  }
 
 
 }
