913dcf1554574d26a9ee1e6e4e96e8c202b48548
Peter Becich <peterbecich@gmail.com>
2018-01-06 23:03:41 -0800

    json encoder for twitter histogram (with generic key) needs work

--- src/main/scala/me/peterbecich/bannodemo/twitter/stats/TwitterHistogram.scala
+++ src/main/scala/me/peterbecich/bannodemo/twitter/stats/TwitterHistogram.scala
@@ -12,10 +12,14 @@ import fs2.async.immutable.{Signal => ISignal}
 import com.danielasfregola.twitter4s.entities.Tweet
 
 import scala.collection.concurrent.TrieMap
+import scala.collection.immutable.Map
 import scala.collection.immutable.HashSet
+import scala.collection.immutable.IndexedSeq
 
 import java.time.{LocalDateTime, ZoneOffset, Duration}
 import java.time.temporal.ChronoUnit
+
+import io.circe.generic.encoding.DerivedObjectEncoder
 
 import me.peterbecich.bannodemo.twitter.TwitterStats.getTweetTime
 
@@ -25,8 +29,37 @@ import scala.concurrent.duration._
 
 object TwitterHistogram {
 
-  type Histogram[K] = TrieMap[K, Long]
+  type Histogram[K] = (IndexedSeq[K], TrieMap[K, Long])
   type HistogramSignal[K] = Signal[IO, Histogram[K]]
+
+  private def makeHistogramSignal[K]: IO[HistogramSignal[K]] =
+    Signal.apply((IndexedSeq.empty[K], TrieMap.empty[K, Long]))(IO.ioEffect, global)
+
+  def makeTwitterHistogram[K : DerivedObjectEncoder]
+    (_name: String, _predicate: Tweet => K, _bins: Set[K]):
+      IO[TwitterHistogram[K]] = for { 
+    _histogramSignal <- makeHistogramSignal[K]
+    val bins = (new HashSet()) ++ _bins
+  } yield {
+    new TwitterHistogram[K](_name, _histogramSignal, _predicate, bins) {}
+  }
+  
+}
+
+import TwitterHistogram._
+
+abstract class TwitterHistogram[K : io.circe.Encoder]
+  (
+    val name: String,
+    val histogramSignal: HistogramSignal[K],
+    val predicate: Tweet => K,
+    val bins: HashSet[K]
+  )
+  (implicit val __kEncoder: DerivedObjectEncoder[K]) {
+
+  // lazy implicit val kEncoder: DerivedObjectEncoder[K] = _kEncoder
+
+  import TwitterHistogram._
 
   object JSON {
     import io.circe._
@@ -34,55 +67,44 @@ object TwitterHistogram {
     import io.circe.syntax._
     import io.circe.literal._
     import io.circe.generic.semiauto._
+    // import io.circe.generic.auto._
+
 
     import me.peterbecich.bannodemo.JSON.Common._
 
-    // implicit val averagePayloadEncoder: Encoder[AveragePayload] = deriveEncoder
-
-    case class HistogramPayload[K](
-      name: String
+    case class HistogramPayload(
+      name: String,
+      histogram: Map[K, Long]
     )
 
-    def makeHistogramPayload[K](
-      name: String
-    ): HistogramPayload[K] =
-      HistogramPayload[K](name)
-  }
+    // TODO
+    implicit val doe: DerivedObjectEncoder[HistogramPayload] = ???
 
-  private def makeHistogramSignal[K]: IO[HistogramSignal[K]] =
-    Signal.apply(TrieMap.empty[K, Long])(IO.ioEffect, global)
+    implicit lazy val histogramPayloadEncoder: Encoder[HistogramPayload] =
+      deriveEncoder[HistogramPayload]
+    
+    def makeHistogramPayload( _name: String, _hist: Map[K, Long] ):
+        HistogramPayload =
+      HistogramPayload(_name, _hist)
 
-  def makeTwitterHistogram[K](_name: String, _predicate: Tweet => K, _bins: Set[K]):
-      IO[TwitterHistogram[K]] = for { 
-    _histogramSignal <- makeHistogramSignal[K]
-  } yield {
-    new TwitterHistogram[K] {
-      val name = _name
-      val histogramSignal = _histogramSignal
-      val predicate = _predicate
-      val bins = (new HashSet()) ++ _bins
-     }
+    def makeHistogramPayloadJson( _name: String, _hist: Map[K, Long] ):
+        io.circe.Json =
+      makeHistogramPayload(_name, _hist).asJson
+
   }
   
-}
 
-abstract class TwitterHistogram[K] {
-
-  import TwitterHistogram._
-
-  val name: String
-  val histogramSignal: HistogramSignal[K]
-  val predicate: Tweet => K
-
-  val bins: HashSet[K]
+  private def sortBins(bins: IndexedSeq[K], histogram: TrieMap[K, Long]): IndexedSeq[K] =
+    bins.sortBy[Long]{ (k: K) => histogram.getOrElse(k, 0) }.reverse
 
   private def incrementKey(tweet: Tweet): IO[Unit] = {
     val key: K = predicate(tweet)
     if(bins.contains(key)) {
-      histogramSignal.modify { histogram =>
+      histogramSignal.modify { case (bins, histogram) =>
         val count: Long = histogram.getOrElse(key, 0)
-        val _histogram: Histogram[K] = histogram += ((key, count+1))
-        _histogram
+        val _histogram: TrieMap[K, Long] = histogram += ((key, count+1))
+        val _bins = sortBins(bins, _histogram)
+        (_bins, _histogram)
       }.map(_ => ())
     } else IO (())
   }
